03a9b36de8d6667c83d831383ee7faef23504277
OndrejFixmer <73097549+OndrejFixmer@users.noreply.github.com>
2021-11-26 15:00:13 +0100

    Delete Main.java

--- src/main/java/cz/educanet/lights/out/Main.java
+++
@@ -1,9 +0,0 @@
-package cz.educanet.lights.out;
-
-import cz.educanet.lights.out.presentation.Window;
-
-public class Main {
-    public static void main(String[] args) {
-        Window.main(args);
-    }
-}
